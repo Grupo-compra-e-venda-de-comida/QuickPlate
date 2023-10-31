6ed2ab000077cbe1aea8e62cad6a28a4fe7d6b1c
EliasBaran <74542335+EliasBaran@users.noreply.github.com>
2023-10-31 13:54:53 -0300

    Atualização

--- QuickPlate-main/app/view/home/indexCliente.php
+++ QuickPlate-main/app/view/home/indexCliente.php
@@ -3,11 +3,12 @@ require_once(__DIR__ . "/../include/header.php");
 ?>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
-<link href="../css/sticky-footer.css" rel="stylesheet">
 
-<div class="container-fluid pb-5" style="margin-top: 270px; margin-bottom: 75px;">
+<link href="../css/sticky-footer.css" rel="stylesheet">
+<link href="../css/estiloMenu.css" rel="stylesheet">
+<link href="../css/app.css" rel="stylesheet">
+
+<div class="container-fluid pb-5" style="padding-top: 120px; margin-bottom: 75px;">
 
   <!-- Navbar -->
   <div class="row">
@@ -16,36 +17,55 @@ require_once(__DIR__ . "/../include/header.php");
     ?>
   </div>
 
+  <div class="container-fluid">
 
-  <!-- Linha Preta -->
-  <div id="linha-horizontal2" style=" position: absolute; width: 100%; border: 1px solid #000; bottom: 24%; left:0px"></div>
+    <!-- Card de Vendedores -->
+    <div class="text-lg-start text-muted">
 
-  <!-- Botões de Vendedores -->
-  <div>
-    <div class="text-lg-start bg-light text-muted" style="position: absolute; top:25%; width:100%; height:100%; left:1px">
-
-      <div style="margin: auto; text-align: center;" class="m-2">
-        <h2>VENDEDORES</h2>
+      <div class="page-title m-4">
+        <h2><span>Selecione seus</span> Vendedores</h2>
       </div>
 
-      <div class="row row-cols-1 row-cols-md-3 g-4" style="margin-right: 0px; margin-left: 1px">
+      <div class="row">
         <?php foreach ($dados['listaVendedores'] as $vend) : ?>
-          <div class="col">
-            <div class="card h-100 w-100" style="background: #f8f9fa;">
+
+          <div class="col-md-3">
+            
+            <div class="card mb-3" style="background: #f8f9fa;">
               <div class="card-body">
                 <h4 class="card-title"><?= $vend->getNome() ?></h4>
                 <p class="card-text">Avaliação</p>
+
+                <div id="rating" class="star-rating">
+
+                  <!-- ALTERAR O VALOR 3 PARA A VARIÁVEL DE AVALIAÇÃO DO VENDEDOR -->
+                  <?php for ($i = 0; $i < 3; $i++) : ?>
+                    <span class="rating-star">&#9733;</span>
+                  <?php endfor; ?>
+                </div>
+
+
               </div>
               <div class="card-footer">
-                <small class="text-body-secondary-center"><a class="btn btn-outline-success col-4 ml-3 mt-2" href="pedidoController.php?action=listProdVend&idVendedor=<?= $vend->getIdVendedor(); ?>">Iniciar Pedido</a></small>
+                <small class="text-body-secondary-center"><a class="btn btn-outline-success" href="pedidoController.php?action=listProdVend&idVendedor=<?= $vend->getIdVendedor(); ?>">Iniciar Pedido</a></small>
               </div>
             </div>
+
           </div>
+
         <?php endforeach; ?>
       </div>
     </div>
+
+
   </div>
+<<<<<<< HEAD
+
+
+
+=======
   <div id="linha-horizontal2" style=" position: fixed; width: 100%; border: 1px solid #000; bottom: 8%; left:0px"></div>
+>>>>>>> 4dcc0eb284727a28be3115f8e7c8e1e6f43325a8
   <!--Footer-->
   <?php
   require_once(__DIR__ . "/../include/footer.php");
